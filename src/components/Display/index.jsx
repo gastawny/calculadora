2b500ea3acad094dc0218441cef741fa1507922b
gastawnyl <gastawny@gmail.com>
2023-01-12 12:37:53 -0300

    Visual do componente Display finalizado

--- src/components/Display/index.jsx
+++ src/components/Display/index.jsx
@@ -1,11 +1,17 @@
 import './Display.css'
 import Toggle from '../Toggle'
+import { AiOutlineHistory } from 'react-icons/ai'
 
 const Display = () => {
     return (
         <div className='Display'>
-            <Toggle />
-            <h1 className='resultado'>4002</h1>
+            <div className='toggle'><Toggle /></div>
+            <button className='btn-history'>
+                <AiOutlineHistory className='history' />
+            </button>
+            <input readOnly type="text" className='expressao' value={'1000x4+2x1'} />
+            <h1 className='igual'>=</h1>
+            <input readOnly type="text" className='resultado' value={4002} />
         </div>
     )
 }
